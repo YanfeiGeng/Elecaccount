4c56e635f52d3926faab9206b2543810d4dacd41
Ivan Geng.OF <gz.pratt@gmail.com>
2011-05-10 23:28:42 +0800

    Correct validation for add consume record form.

--- src/com/ivan/consume/AddConsumeRecord.java
+++ src/com/ivan/consume/AddConsumeRecord.java
@@ -23,6 +23,8 @@ public class AddConsumeRecord extends Activity{
 	
 	private Spinner consumeCate;
 	
+	private String[] cates = {"食品", "衣服鞋子", "生活用品", "娱乐休闲", "工作学习", "其他"};;
+	
 	private DatePicker consumeDate;
 	
 	private EditText consumePrice;
@@ -46,13 +48,39 @@ public class AddConsumeRecord extends Activity{
 
 			@Override
 			public void onClick(View arg0) {
-				AddConsumeRecord.this.showMsg(AddConsumeRecord.this.consumeName.getText().toString());
-				System.out.println(AddConsumeRecord.this.consumeName.getText().toString());
-//				AddConsumeRecord.this.showMsg(AddConsumeRecord.this.consumeCate.getSelectedItem() + ", " + AddConsumeRecord.this.consumeCate.getSelectedItemId());
-//				AddConsumeRecord.this.showMsg(AddConsumeRecord.this.consumeDate.getYear() + "-" + AddConsumeRecord.this.consumeDate.getMonth() + "-" + AddConsumeRecord.this.consumeDate.getDayOfMonth());
-//				AddConsumeRecord.this.showMsg(AddConsumeRecord.this.consumePrice.getText().toString());
-//				AddConsumeRecord.this.showMsg(AddConsumeRecord.this.consumeQuntity.getText().toString());
-//				AddConsumeRecord.this.showMsg(AddConsumeRecord.this.consumeComments.getText().toString());
+				String name = AddConsumeRecord.this.consumeName.getText().toString();
+				String category = AddConsumeRecord.this.consumeCate.getSelectedItem().toString();
+				long cate_A = AddConsumeRecord.this.consumeCate.getSelectedItemId();
+				String date = AddConsumeRecord.this.consumeDate.getYear() + "-" + (AddConsumeRecord.this.consumeDate.getMonth()+1) + "-" + AddConsumeRecord.this.consumeDate.getDayOfMonth();
+				String price = AddConsumeRecord.this.consumePrice.getText().toString();
+				String quntity = AddConsumeRecord.this.consumeQuntity.getText().toString();
+				String comments = AddConsumeRecord.this.consumeComments.getText().toString();
+				Object[][] values = {{name, cate_A, date, price, quntity}, 
+							{getResources().getString(R.string.add_conlabel_name), getResources().getString(R.string.add_conlabel_cate), getResources().getString(R.string.add_conlabel_date),
+							getResources().getString(R.string.add_conlabel_price), getResources().getString(R.string.add_conlabel_quntity)}};
+				boolean isValidate = true;
+				for(int i = 0; i < values[0].length; i++){
+					System.out.println("Outer: Come in....." + values[1][i]);
+					if(values[0][i] == null || "".equals(values[0][i].toString())) {
+						isValidate = false;
+						System.out.println("Come in....." + values[1][i] + ", " + values[0][i]);
+						AddConsumeRecord.this.showMsg("Field: " + ((String)values[1][i]).replace("：", "") + " can not be empty!");
+						break;
+					}
+				}
+				
+				if(isValidate){
+					//Do sth...
+					
+					
+				}
+				System.out.println(name);
+				System.out.println(category);
+				System.out.println(cate_A);
+				System.out.println(date);
+				System.out.println(price);
+				System.out.println(quntity);
+				System.out.println(comments);
 			}
 			
 		};
@@ -79,14 +107,14 @@ public class AddConsumeRecord extends Activity{
 	 * Initialize consume category drop down list.
 	 */
 	public void initConsuCate(){
-		String[] cates = {"食品", "衣服鞋子", "生活用品", "娱乐休闲", "工作学习", "其他"};
 		List<String> catesValues = new ArrayList<String>();
 		for(int i = 0; i < cates.length; i++){
 			catesValues.add(cates[i]);
 		}
 		
-		ArrayAdapter<String> cagetories = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item , catesValues);
-		this.consumeCate.setAdapter(cagetories);
+		ArrayAdapter<String> categories = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item , catesValues);
+		categories.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		this.consumeCate.setAdapter(categories);
 	}
 	
 	
@@ -96,16 +124,6 @@ public class AddConsumeRecord extends Activity{
 	private void showMsg(String msg){
 		Toast.makeText(AddConsumeRecord.this, msg, Toast.LENGTH_SHORT).show();
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 
 	public EditText getConsumeName() {
